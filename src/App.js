fd395d3127e602313045416bbb80c6ee184e0588
mohammadamin <mohammadaminweb@gmail.com>
2025-01-12 12:05:42 +0330

    editing small spaces

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import react from 'react';
 import SubmitCompo from './components/to-do-compo.jsx';
 
+
 function App() {
+
   return (
     
+    <div>
     <SubmitCompo/>
-    
+    </div>
   );
 }
+
 
 export default App;
